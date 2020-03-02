a3408967c6a545ba67339df200378fcc43000e8a
ekpliska <pliska.ek@gmail.com>
2020-03-02 20:37:01 +0300

    added: switch month at the header organizer:

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { DateService } from '../shared/date.service';
 
 @Component({
-  selector: 'app-selector',
-  templateUrl: './selector.component.html',
-  styleUrls: ['./selector.component.scss']
+	selector: 'app-selector',
+	templateUrl: './selector.component.html',
+	styleUrls: ['./selector.component.scss']
 })
 export class SelectorComponent {
 
-  constructor(private dateService: DateService) { }
+	constructor(private dateService: DateService) { }
+
+	switchMonth(step: number) {
+		this.dateService.changeMonth(step);
+	}
 
 }
--- src/app/shared/date.service.ts
+++ src/app/shared/date.service.ts
@@ -10,4 +10,13 @@ export class DateService {
 
     public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment());
 
+    /**
+     * Изменение месяца в календаре
+     * @param step Шаг для переключения месяца (1 следующий или -1 предыдущий)
+     */
+    changeMonth(step: number) {
+        const value = this.date.value.add(step, 'month');
+        this.date.next(value);
+    }
+
 }
